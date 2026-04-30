1993ab77b622c17701427104f73971667e199b8b
igorf08 <igorfmaa@gmail.com>
2026-04-30 08:31:23 -0300

    fix: Corrige patributos faltantes na tabela place_item

--- src/main/resources/db.migration/V1__create_place_item_table.sql
+++ src/main/resources/db.migration/V1__create_place_item_table.sql
@@ -1,6 +1,7 @@
 -- Migration responsável pela criação da tabela place_item com suas respectivas colunas.
 CREATE TABLE tb_placeitem (
-    nome varchar(128);
-    categoria varchar(128);
-    dataViagem date;
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    categoria CATEGORIA(100) NOT NULL,
+    dataViagem DATE NOT NULL;
 );
